ab9032825a6499df84d7fad46a978a9e7dcda392
Tuan Phan <tuan.phan@titancorpvn.com>
2022-06-20 16:49:21 +0700

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { IconButton } from '@mui/material'
+import Icon from '@mui/material/Icon';
 
 const Header = () => {
 
   return (
     <div className='header' >
-      <Link to='/'>DashBoard</Link> &nbsp;
+      <IconButton color="primary" size="large"><Icon>home</Icon></IconButton>
+      
+      {/* <Link to='/'>DashBoard</Link> &nbsp;
       <Link to='/about'>About</Link> &nbsp;
-      <Link to='/detail'>Detail</Link>
+      <Link to='/detail'>Detail</Link> */}
     </div>
   )
 }
